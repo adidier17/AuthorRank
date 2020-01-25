f7d28c34cdc632fd465b42ff282b9ed574d36e25
vc1492a <vc1492a@gmail.com>
2020-01-24 21:26:27 -0800

    simple examples

---
+++ examples/export_to_json.py
@@ -0,0 +1,16 @@
+# imports
+from AuthorRank.graph import create, export_to_json
+import json
+
+
+# read in sample json
+with open("../data/author_network.json", 'r') as f:
+    data = json.load(f)
+
+# generate a graph
+G = create(documents=data['documents'])
+
+# export them
+export = export_to_json(graph=G)
+
+print(json.dumps(export, indent=4))

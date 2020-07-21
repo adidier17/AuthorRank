6a164b5e14c3dd8bb147438ac7d2580cb73d9cd0
vc1492a <vc1492a@gmail.com>
2020-07-21 16:39:14 -0700

    fix name of library in version check

--- author_rank/__init__.py
+++ author_rank/__init__.py
@@ -11,7 +11,7 @@ See https://github.com/adidier17/AuthorRank.
 
 import sys
 if sys.version_info[:2] < (3, 5):
-    m = "Python 3.5 or later is required for NetworkX (%d.%d detected)."
+    m = "Python 3.5 or later is required for AuthorRank (%d.%d detected)."
     raise ImportError(m % sys.version_info[:2])
 del sys
 
